fdbc2fcb47261aeb883bb82004d79c9e244a630b
Nitish Garg <nitish.garg.6174@gmail.com>
2016-10-30 03:10:47 +0530

    Completed piano note pressing

--- js/main.js
+++ js/main.js
@@ -1,22 +1,34 @@
-bufferSize = 512;
-numberOfInputChannels = 1;
-numberOfOutputChannels = 1;
+/* Load keys JSON data */
+var keys = JSON.parse(keys);
 
-playbackRate = 1;
+/* Constants */
+var bufferSize = 512;
+var numberOfInputChannels = 1;
+var numberOfOutputChannels = 1;
 
-frequencyBinCount = 256;
-smoothingTimeConstant = 0.9;
-fftSize = 4096;
-minDecibels = -100;
-maxDecibels = -10;
+var playbackRate = 1;
 
-/*
-	each bin freq range = sample rate / fftSize
-*/
+var frequencyBinCount = 256;
+var smoothingTimeConstant = 0.9;
+var fftSize = 4096;
+var minDecibels = -100;
+var maxDecibels = -10;
 
+/* Keys */
+var tracks = 1;
+var threshold = 127;
+var min_note_no = 13;
+var max_note_no = 76;
+
+var keys_len = keys.length;
+var max_key_freq = keys[keys_len-max_note_no]["frequency"];
+var min_key_freq = keys[keys_len-min_note_no]["frequency"];
+var freq_map = {};
+
+
+/* MP3 processing */
 var visualizer;
 var out_elem = document.querySelector('#output div.row');
-var keys = JSON.parse(keys);
 
 
 $(document).ready(function () {
@@ -57,13 +69,14 @@ AudioVisualizer.prototype.setupAudioProcessing = function () {
 	//analyser to speakers
 	this.analyser.connect(this.javascriptNode);
 	//connect source to analyser
-	this.sourceBuffer.connect(this.audioContext.destination);
+	// Uncomment the below line to play mp3 file
+	// this.sourceBuffer.connect(this.audioContext.destination);
 
 	for(var i=0;i<frequencyBinCount;i++)
 	{
-		// var label = i;
 		var label = getBinFrequency(this.sampleRate,i);
-		out_elem.innerHTML += "<div class='col-sm-1'><span>"+label.toString()+" : </span><span id='bin"+i.toString()+"'></span></div>";
+		out_elem.innerHTML += "<div class='col-sm-1'><span id='freq"+i.toString()+"'>"+label.toString()+"</span> : <span id='bin"+i.toString()+"'></span></div>";
+		addMapping(label);
 	}
 
 	var that = this;
@@ -78,9 +91,18 @@ AudioVisualizer.prototype.setupAudioProcessing = function () {
 		for(var i=0;i<frequencyBinCount;i++)
 		{
 			var elem = document.getElementById('bin'+i.toString());
+			f = parseInt(document.getElementById('freq'+i.toString()).innerHTML);
+			if(freq_map[f]!=undefined)
+			{
+				note = freq_map[f];
+				if(array[i]>=threshold)
+					$("[data-ipn='"+note+"']").addClass('active');
+				else
+					$("[data-ipn='"+note+"']").removeClass('active');
+			}
+
 			elem.innerHTML = array[i];
 		}
-		// console.log(array);
 	}
 
 };
@@ -151,4 +173,23 @@ function getBinFrequency(sampleRate,binIndex)
 	binWidth = sampleRate/fftSize;
 	f = binWidth*binIndex;
 	return Math.round(f);
+}
+
+function addMapping(frequency)
+{
+	if(frequency>=min_key_freq && frequency<=max_key_freq)
+	{
+		var pos = keys_len-max_note_no;
+		var mindiff = Math.abs(frequency-keys[pos]['frequency']);
+		for(var i=pos;i<=keys_len-min_note_no;i++)
+		{
+			var diff = Math.abs(frequency-keys[i]['frequency']);
+			if(diff<mindiff)
+			{
+				pos=i;
+				mindiff=diff;
+			}
+		}
+		freq_map[frequency] = keys[pos]["note"];
+	}
 }
