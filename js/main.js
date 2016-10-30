2612ebd643611716e11565d6672410d67535bb7f
Nitish Garg <nitish.garg.6174@gmail.com>
2016-10-30 07:54:16 +0530

    Improve conversion using average amplitude thresholding

--- js/main.js
+++ js/main.js
@@ -1,30 +1,46 @@
+/* Play mode */
+// var playMode = "song"
+var playMode = "piano"
+
 /* Load keys JSON data */
 var keys = JSON.parse(keys);
 
 /* Constants */
-var bufferSize = 512;
+var bufferSize = 256;
 var numberOfInputChannels = 1;
 var numberOfOutputChannels = 1;
 
 var playbackRate = 1;
 
 var frequencyBinCount = 256;
-var smoothingTimeConstant = 0.9;
 var fftSize = 4096;
+// var binWidth = 10;
+var sampleRate = 44100;
+// var sampleRate = binWidth*fftSize;
 var minDecibels = -100;
 var maxDecibels = -10;
+var smoothingTimeConstant = 0.9;
 
 /* Keys */
 var tracks = 1;
-var threshold = 127;
+var history_size = 5;
 var min_note_no = 13;
 var max_note_no = 76;
+var duration = 4;
 
 var keys_len = keys.length;
 var max_key_freq = keys[keys_len-max_note_no]["frequency"];
 var min_key_freq = keys[keys_len-min_note_no]["frequency"];
 var freq_map = {};
 
+var history = new Array(history_size);
+for (var i=0;i<history_size;i++)
+{
+	history[i] = new Array(frequencyBinCount);
+	for (var j=0;j<frequencyBinCount;j++)
+		history[i][j] = 0;
+}
+history_pos = 0;
 
 /* MP3 processing */
 var visualizer;
@@ -35,6 +51,13 @@ $(document).ready(function () {
 	visualizer = new AudioVisualizer();
 	visualizer.setupAudioProcessing();
 	visualizer.handleDrop();
+	MIDI.loadPlugin({
+		soundfontUrl: "./soundfont/",
+		instrument: "acoustic_grand_piano",
+		onsuccess: function() {			
+			playHardCoded();
+		}
+	});
 });
 
 
@@ -44,13 +67,12 @@ function AudioVisualizer() {
 	this.audioContext;
 	this.sourceBuffer;
 	this.analyser;
-	this.sampleRate;
 }
 
 AudioVisualizer.prototype.setupAudioProcessing = function () {
 	//get the audio context
 	this.audioContext = new AudioContext();
-	this.sampleRate = this.audioContext.sampleRate;
+	this.audioContext.sampleRate = sampleRate;
 	//create the javascript node
 	this.javascriptNode = this.audioContext.createScriptProcessor(bufferSize,numberOfInputChannels,numberOfOutputChannels);
 	this.javascriptNode.connect(this.audioContext.destination);
@@ -69,12 +91,12 @@ AudioVisualizer.prototype.setupAudioProcessing = function () {
 	//analyser to speakers
 	this.analyser.connect(this.javascriptNode);
 	//connect source to analyser
-	// Uncomment the below line to play mp3 file
-	// this.sourceBuffer.connect(this.audioContext.destination);
+	if(playMode=="song")
+		this.sourceBuffer.connect(this.audioContext.destination);
 
 	for(var i=0;i<frequencyBinCount;i++)
 	{
-		var label = getBinFrequency(this.sampleRate,i);
+		var label = getBinFrequency(i);
 		out_elem.innerHTML += "<div class='col-sm-1'><span id='freq"+i.toString()+"'>"+label.toString()+"</span> : <span id='bin"+i.toString()+"'></span></div>";
 		addMapping(label);
 	}
@@ -88,21 +110,39 @@ AudioVisualizer.prototype.setupAudioProcessing = function () {
 		var array = new Uint8Array(that.analyser.frequencyBinCount);
 		that.analyser.getByteFrequencyData(array);
 
+		var slice_sum = 0;
+		var slice_count = 0;
 		for(var i=0;i<frequencyBinCount;i++)
 		{
+			var f = parseInt(document.getElementById('freq'+i.toString()).innerHTML);
 			var elem = document.getElementById('bin'+i.toString());
-			f = parseInt(document.getElementById('freq'+i.toString()).innerHTML);
+			if(freq_map[f]!=undefined)
+			{
+				slice_sum += array[i];
+				slice_count++;
+				elem.innerHTML = array[i];
+				history[history_pos][i] = array[i];
+			}
+		}
+		var slice_avg = slice_sum/slice_count;
+		for(var i=0;i<frequencyBinCount;i++)
+		{
+			var f = parseInt(document.getElementById('freq'+i.toString()).innerHTML);
 			if(freq_map[f]!=undefined)
 			{
 				note = freq_map[f];
-				if(array[i]>=threshold)
+				if(checkDisplayNote(slice_avg,history,i)==1)
 					$("[data-ipn='"+note+"']").addClass('active');
 				else
 					$("[data-ipn='"+note+"']").removeClass('active');
+				if(playMode=="piano")
+				{
+					if(checkPlayNote(slice_avg,history,i)==1)
+						playNote(note);
+				}
 			}
-
-			elem.innerHTML = array[i];
 		}
+		history_pos=(history_pos+1)%history_size;
 	}
 
 };
@@ -168,7 +208,7 @@ AudioVisualizer.prototype.handleDrop = function () {
 }
 
 
-function getBinFrequency(sampleRate,binIndex)
+function getBinFrequency(binIndex)
 {
 	binWidth = sampleRate/fftSize;
 	f = binWidth*binIndex;
@@ -192,4 +232,73 @@ function addMapping(frequency)
 		}
 		freq_map[frequency] = keys[pos]["note"];
 	}
+}
+
+
+function playHardCoded()
+{
+	// playNote('C4');
+	// playNote('E4');
+	// playNote('G4');
+	// playNote('A4');
+	// playNote('C5');
+	// playNote('E5');
+}
+
+function playNote(note)
+{
+	if(typeof note === 'string')
+		note = keyToNote(note);
+	MIDI.noteOn(0, note, 127, 0);
+	MIDI.noteOff(0, note, 0.3);
+}
+
+function keyToNote(key)
+{
+	var l = key.length-1;
+	var rawKey = key.substr(0,l);
+	var octave = parseInt(key[l]);
+	return (octave*12)+keyNoteMap[rawKey];
+}
+
+var keyNoteMap = {
+	'A':1,
+	'A#':2,
+	'B':3,
+	'C':4,
+	'C#':5,
+	'D':6,
+	'D#':7,
+	'E':8,
+	'F':9,
+	'F#':10,
+	'G':11,
+	'G#':12
+};
+
+function checkDisplayNote(slice_avg,history,i)
+{
+	var amp = history[history_pos][i];
+	var l = history[history_pos].length;
+	if( amp>1.3*slice_avg+50 )
+	{
+		if( i==l||history[history_pos][i]>history[history_pos][i+1] )
+			return 1;
+	}
+	return 0;
+}
+
+function checkPlayNote(slice_avg,history,i)
+{
+	var amp = history[history_pos][i];
+	var l = history[history_pos].length;
+	var score1 = amp-1.25*slice_avg-30;
+	var score2 = amp-history[(history_pos+1)%history_size][i];
+	var score = score1 + score2;
+	if( amp>1.3*slice_avg+50 )
+	{
+		if(amp-history[(history_pos+1)%history_size][i]>=10)
+			return 1;
+	}
+	return 0;
 }
--- js/makekeyboard.js
+++ js/makekeyboard.js
@@ -1,4 +1,3 @@
 var container = document.querySelector('.piano-container');
-// initial setup
 piano(container, {octaves: 5});
 container.classList.add('piano-show-names');
